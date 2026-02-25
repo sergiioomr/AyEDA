5482a1d1a461729433193b52ee1a8ab6cf5c6368
sergiioomr <alu0101718194@ull.edu.es>
2026-02-25 07:51:39 +0000

    Directory modification created to do the practice

---
+++ p2_hormiga_langton/p2_modificacion/build/CMakeFiles/langton.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/langton.dir/src/ant.cc.o"
+  "CMakeFiles/langton.dir/src/ant.cc.o.d"
+  "CMakeFiles/langton.dir/src/ant_DDII.cc.o"
+  "CMakeFiles/langton.dir/src/ant_DDII.cc.o.d"
+  "CMakeFiles/langton.dir/src/ant_IDID.cc.o"
+  "CMakeFiles/langton.dir/src/ant_IDID.cc.o.d"
+  "CMakeFiles/langton.dir/src/main.cc.o"
+  "CMakeFiles/langton.dir/src/main.cc.o.d"
+  "CMakeFiles/langton.dir/src/simulator.cc.o"
+  "CMakeFiles/langton.dir/src/simulator.cc.o.d"
+  "CMakeFiles/langton.dir/src/tape.cc.o"
+  "CMakeFiles/langton.dir/src/tape.cc.o.d"
+  "langton"
+  "langton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/langton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ p2_hormiga_langton/p2_modificacion/include/simulator.h
@@ -0,0 +1,43 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Algoritmos y Estructuras de Datos Avanzadas
+ * 
+ * @file simulator.h
+ * @author Sergio Molina Ríos (alu0101718194@ull.edu.es)
+ * @date 2026-02-05
+ * @brief That file defines the Langton's Ant Simulator class. 
+ *        That class will be use to make the simulation with the tape and the ant_x classes
+ */
+            
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+#include "../include/ant.h"
+#include "../include/tape.h"
+#include "../include/colors.h"
+#include "../include/enum_class.h"
+#include "../include/ant_DDII.h"
+#include "../include/ant_IDID.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <memory>
+
+class Simulator {
+  public:
+  // Constructor
+    Simulator(const std::string &filename);
+
+    void PrintTapeAnt();
+    void Simulation();
+
+  private:
+    Tape tape_;
+    std::vector<std::unique_ptr<Ant>> ants_;
+    int num_colors_;
+    void Export();
+};
+
+#endif // SIMULATOR_H
