f93df8de1a9da1739c2cf939e0ad6029d866defa
Sergio <alu0101718194@ull.edu.es>
2026-04-07 19:02:05 +0100

    StaticSequence done. Sort method started

---
+++ p5_sort/p5_modificacion/include/sort_method.h
@@ -0,0 +1,26 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Algoritmos y Estructuras de Datos Avanzadas
+ * @file sort_method.h
+ * @author Sergio Molina Ríos (alu0101718194@ull.edu.es)
+ * @date 2026-04-07
+ * @brief 
+ */
+
+#ifndef SORT_METHOD_H
+#define SORT_METHOD_H
+
+#include "static_sequence.h"
+
+template <class Key>
+class SortMethod {
+  public:
+    SortMethod() {}
+    virtual void Sort() = 0;
+  private:
+    StaticSequence<Key> &sequence_;
+};
+
+#endif // SORT_METHOD_H
---
+++ p5_sort/p5_modificacion/include/static_sequence.h
@@ -0,0 +1,62 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Algoritmos y Estructuras de Datos Avanzadas
+ * @file static_sequence.h
+ * @author Sergio Molina Ríos (alu0101718194@ull.edu.es)
+ * @date 2026-04-07
+ * @brief 
+ */
+#ifndef STATIC_SEQUENCE_H
+#define STATIC_SEQUENCE_H
+
+#include "sequence.h"
+
+// Derived class. Implement the Static Sequence
+template<class Key>
+class StaticSequence : Sequence<Key> {
+  public:
+    StaticSequence(const unsigned size) : size_(size) {}
+  
+    virtual Key operator[](const unsigned index) const {
+      return data_[index];
+    }
+
+    virtual Key& operator[](const unsigned index) {
+      return data_[index];
+    }
+    // Search a given key in the vector. Return true or false if it could be found or not
+    virtual bool search(const Key &k) const override {
+
+      for (unsigned i = 0; i < data_.size(); i++) {
+        if (data_[i] == k) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    // Insert a given key in the vector. Return true or false if it could be found or not
+    virtual bool insert(const Key &k) override {
+
+      if ((!IsFull()) && (!search(k))) {
+        data_.push_back(k);
+        return true;
+      }
+      
+      return false;
+    }
+
+    // Return if the vector is full
+    virtual bool IsFull() const {
+      return data_.size() >= size_;
+    }
+
+  private:
+    unsigned size_;
+    std::vector<Key> data_;
+};
+
+#endif // STATIC_SEQUENCE_H
